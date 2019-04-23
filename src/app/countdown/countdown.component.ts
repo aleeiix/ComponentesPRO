d3765b45b66af510ffeebb994dbbd949ec459a02
Aleix Riba <aleixriba33@gmail.com>
2019-04-23 20:08:32 +0200

    complete basic level

--- src/app/countdown/countdown.component.ts
+++ src/app/countdown/countdown.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter, OnDestroy, OnChanges, SimpleChange, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-countdown',
   templateUrl: './countdown.component.html',
   styleUrls: ['./countdown.component.scss']
 })
-export class CountdownComponent{
+export class CountdownComponent implements OnInit, OnDestroy, OnChanges{
+  
+  @Output() onDecrease = new EventEmitter<number>();
+  @Output() onComplete = new EventEmitter<void>();
+
+  @Input() init: number = null;
+  public counter: number = 0;
+  private countdownTimerRef: any = null;
 
   constructor() { }
+  
+  ngOnInit(): void {
+    this.startCountdown();
+  }
+
+  ngOnDestroy(): void {
+    this.clearTimeout();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('init value update to: ', changes.init.currentValue);
+    this.startCountdown();
+  }
+
+
+  startCountdown(): void {
+    if (this.init && this.init > 0) {
+      this.clearTimeout();
+      this.counter = this.init;
+      this.doCountdown();
+    }
+  }
+
+  doCountdown(): void {
+    this.countdownTimerRef = setTimeout(() => {
+      this.counter--;
+      this.processCount();
+    }, 1000);
+  }
+
+  processCount(): void {
+    this.onDecrease.emit(this.counter);
+    console.log('count is ', this.counter);
+
+    if (this.counter === 0) {
+      this.onComplete.emit();
+      console.log('-- COUNTER END --');
+    } else {
+      this.doCountdown();
+    }
+  }
+
+  private clearTimeout(): void {
+    if (this.countdownTimerRef) {
+      clearTimeout(this.countdownTimerRef);
+      this.countdownTimerRef = null;
+    }
+  }
 
 }
