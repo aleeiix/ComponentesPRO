81aab0cb2dd18ba4daea51cb8bdbfe9d68f2e423
Aleix Riba <aleixriba33@gmail.com>
2019-05-05 11:44:08 +0200

    start intermediate nivel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,17 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  counterProgress: number = 0;
-  totalCountdown: number = 15;
-
   constructor() { }
-
-  updateProgress($event): void {
-    this.counterProgress = (this.totalCountdown - $event) / this.totalCountdown * 100;
-  }
-
-  countdownFinished(): void {
-    console.log('countdown has finished');
-  }
 
 }
